78b33abea6f4de0eae0b68c6869e805300a029f8
Louis Rémus <louisremus@gmail.com>
2017-03-31 19:09:15 +0200

    tree parsing

--- hmm_pcfg_files/parse_pcfg.py
+++ hmm_pcfg_files/parse_pcfg.py
@@ -3,6 +3,8 @@ import csv
 import pandas as pd
 import numpy as np
 from math import exp
+from nltk.app import srparser, rdparser
+from hmm_pcfg_files.tools import read_from_file
 
 hmm_emits_pd = pd.read_csv("hmm_emits", sep="\t", quoting=csv.QUOTE_NONE, header=None)
 distinct_words = set(np.unique(hmm_emits_pd[1]))
@@ -10,28 +12,58 @@ distinct_tags = set(np.unique(hmm_emits_pd[0]))
 
 arabic_pcfg = open("pcfg").read()
 arabic_pcfg = arabic_pcfg.splitlines()
-
+distinct_words_from_pcfg = set()
+# Correcting the source file
 for i, rule in enumerate(arabic_pcfg):
     rule = rule.replace("PRP$", "PRP2")
     split_rule = rule.split("\t")
     if split_rule[1] in distinct_words:
         # we add " " for lexical production (see lab7)
+        distinct_words_from_pcfg.add(split_rule[1])
         if split_rule[1] == '"':
-            split_rule[1] = "'"+split_rule[1]+"'"
+            split_rule[1] = "'" + split_rule[1] + "'"
         else:
             split_rule[1] = '"' + split_rule[1] + '"'
 
     # min for the float is 7.981037055632809e-06 and scientific notation not allowed...
-    split_rule[2] = "%.10f" % exp(float(split_rule[2])) #
+    split_rule[2] = "%.10f" % exp(float(split_rule[2]))  #
 
     float_prob = exp(float(split_rule[2]))
-    arabic_pcfg[i] = split_rule[0]+" -> "+split_rule[1]+" ["+split_rule[2]+"]" # update arabic_pcfg inplace
+    arabic_pcfg[i] = split_rule[0] + " -> " + split_rule[1] + " [" + split_rule[2] + "]"  # update arabic_pcfg inplace
 
-
+distinct_words.symmetric_difference(distinct_words_from_pcfg)
 arabic_pcfg = "\n".join(arabic_pcfg)
-
 nltk_pcfg = nltk.PCFG.fromstring(arabic_pcfg)
 
+test_sent = read_from_file("dev_sents")
+test_sent = [test[0].split(" ") for test in test_sent]
+
+from nltk import parse
+
+parser = parse.RecursiveDescentParser(nltk_pcfg, trace=2)
 
 
-#arabic_pcfg_df = pd.DataFrame(data=arabic_pcfg)
+for p in parser.parse(test_sent[2]):
+    print(p)
+
+list(parser.parse(test))
+output = " ".join([str(p) for p in parser.parse(test)])
+
+for p in parser.parse(test):
+    print(str(p))
+
+# Not working aon
+with open('parses/candidate_parses', 'w') as f:
+    for sentence in test_sent:
+        output = " ".join([p for p in parser.parse(test_sent)])
+        f.write(output + "\n")
+    f.close()
+
+from nltk import Tree, ProbabilisticTree
+
+# Demonstrate tree parsing.
+s = '(S (NP (DT the) (NN cat)) (VP (VBD ate) (NP (DT a) (NN cookie))))'
+t = Tree.fromstring(s)
+print("Convert bracketed string into tree:")
+print(t)
+print(t.__repr__())
